72c78bc2dd54211b953f0ab5c52d624e4114ca09
JXavierEli <sundayjohnxavier47@gmmail.com>
2026-07-08 10:14:06 +0100

    My first try

---
+++ part01-syntax/exercises/intro.py
@@ -0,0 +1,9 @@
+
+user_input_name = input("what is your name?")
+user_input_age = input("how old are you?")
+age = int(user_input_age)
+user_input_height = input("what is your height in meters?")
+height = float(user_input_height)
+print(f"My name is {user_input_name}, I am {age} years old, and I am {height}m tall.")
+future_age = age + 10
+print(future_age)
