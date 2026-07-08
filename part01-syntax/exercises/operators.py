ae83649707e0d904633ed2563a001a6fe743e0a9
JXavierEli <sundayjohnxavier47@gmmail.com>
2026-07-08 10:35:03 +0100

    operators

---
+++ part01-syntax/exercises/operators.py
@@ -0,0 +1,15 @@
+num1 = input("insert first numbers")
+num2 = input("insert second numbers")
+num1n = int(num1)
+num2n = int(num2)
+add = num1n + num2n
+sub = num1n - num2n
+mult = num1n * num2n
+div = num1n / num2n
+print(add)
+print(sub)
+print(mult)
+print(div)
+print(num1n > num2n)
+print(num1n == num2n)
+print(num1n %2 == 0)
