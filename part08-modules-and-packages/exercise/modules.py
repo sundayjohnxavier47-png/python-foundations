9d1b3f901dfdf3cb3ecd2b2fcb0ff3e576532741
jxavier <sundayjohnxavier47@gmail.com>
2026-09-09 10:41:39 +0100

    modules and packages exercise

---
+++ part08-modules-and-packages/exercise/modules.py
@@ -0,0 +1,14 @@
+import random
+print(random.randint(1, 50))
+
+import datetime
+print(datetime.datetime.now())
+
+import requests
+response = requests.get("https://api.github.com")
+print(response.status_code)
+
+response2 = requests.get("https://api.github.com/users/octocat")
+data = response2.json()
+#print(data)
+print(data["name"])
