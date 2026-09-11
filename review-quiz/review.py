a60660bccc251adb9c7d64485a59e02a54fb5092
jxavier <sundayjohnxavier47@gmail.com>
2026-09-11 16:29:59 +0100

    quiz

---
+++ review-quiz/review.py
@@ -0,0 +1,12 @@
+name = "Xavier"
+age = 42
+print(f"My name is {name} and my dog is {age} years old")
+num1 = int(input("choose a number: "))
+num2 = int(input("choose a number: "))
+add = num1 + num2
+diff = num1 - num2
+div = num1 // num2
+print(add)
+print(diff)
+print(div)
+print(num1 % 2 == 0)
