fd2d2b4083864c94b3344c35e3051c295e421241
jxavier <sundayjohnxavier47@gmail.com>
2026-09-16 10:36:01 +0100

    quiz

--- review-quiz/review.py
+++ review-quiz/review.py
@@ -1,28 +1,45 @@
 # Section A
-name = "Xavier"
-age = 42
-print(f"My name is {name} and my dog is {age} years old")
-num1 = int(input("choose a number: "))
-num2 = int(input("choose a number: "))
-add = num1 + num2
-diff = num1 - num2
-div = num1 // num2
-print(add)
-print(diff)
-print(div)
-print(num1 % 2 == 0)
+#name = "Xavier"
+#age = 42
+#print(f"My name is {name} and my dog is {age} years old")
+#num1 = int(input("choose a number: "))
+#num2 = int(input("choose a number: "))
+#add = num1 + num2
+#diff = num1 - num2
+#div = num1 // num2
+#print(add)
+#print(diff)
+#print(div)
+#print(num1 % 2 == 0)
 
 # Section B
 
-for i in range(1, 20+1):
-    if i % 4 == 0:
-        continue
-    print(i)
+#for i in range(1, 20+1):
+ #   if i % 4 == 0:
+  #      continue
+   # print(i)
 
 
 
-word = input("what is the password? ")
-while word != "secret":
-    word = input("what is the password? ")
+#word = input("what is the password? ")
+#while word != "secret":
+ #   word = input("what is the password? ")
 
     
+# Section C
+
+package = ["cook", "sweep", "clean", "wipe"]
+print(package[1])
+print(package[-1])
+
+expense = {
+    "description": "what",
+    "amount" : 90,
+    "category": "type"
+}
+
+for key, value in expense.items():
+    print(key, value)
+
+nums = [10, 20, 30, 40, 50]
+print(nums[1:-1])
